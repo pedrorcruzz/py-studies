86b0a1456e6e036503cdb3a682242642c0b4b475
pedrorcruzz <phrosa.dev@gmail.com>
2025-01-22 19:27:56 -0300

    chore: delete files

--- py-essentials/py-loops/for.py
+++
@@ -1,5 +0,0 @@
-for i in range(0,5):
-   for j in range (0,5):
-      for k in range(0,5):
-         if i == 4 and j == 4 and k == 4:
-             print(f"i: {i}, j: {j}, k: {k}")
--- py-essentials/py-loops/while.py
+++
@@ -1,8 +0,0 @@
-number = 1
-
-while number <=5:
-   print(number)
-   number+=1
-   if number == 2:
-      print("Number is 2, Bye")
-      break
