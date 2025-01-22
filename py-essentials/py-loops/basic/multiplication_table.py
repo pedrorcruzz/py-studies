08fd6ff371ee61058822f98b6df140c6cc174681
pedrorcruzz <phrosa.dev@gmail.com>
2025-01-22 20:07:54 -0300

    feat: add exercise of  multiplication table

---
+++ py-essentials/py-loops/basic/multiplication_table.py
@@ -0,0 +1,25 @@
+def main():
+   while True:
+      print("-" * 30)
+      print("Menu")
+      print("1 - make a multiplication table")
+      print("2 - Leave")
+      print("-" * 30)
+      input_user = int(input("Choice:"))
+   
+      if input_user == 2:
+         print("Bye Bye")
+         break
+      if input_user == 1:
+         input_table_user = int(input("What number do you want? "))
+         result_table = multiplication_table(input_table_user)
+         print(result_table)
+
+ 
+def multiplication_table(number):
+   result = ""
+   for i in range (0, 11):
+       result += f"{number} x {i} = {number * i}\n" 
+   return result
+
+main()
