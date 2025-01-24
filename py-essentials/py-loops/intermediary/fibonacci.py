f2038f454716ca4ce49326e44885831a569da916
pedrorcruzz <phrosa.dev@gmail.com>
2025-01-24 18:05:11 -0300

    feat: add simple example with for

--- py-essentials/py-loops/intermediary/fibonacci.py
+++ py-essentials/py-loops/intermediary/fibonacci.py
@@ -1,8 +1,12 @@
 input_user = int(input("Put your number: "))
 
-a, b = 0,1
+a, b = 0, 1
 
 while a <= input_user:
-   print(a)
-   a, b = b, a + b
-   
+    print(a)
+    a, b = b, a + b
+
+# Example with for
+for _ in range(input_user):
+    print(a)
+    a, b = b, a + b
